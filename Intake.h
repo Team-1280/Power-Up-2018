fe8bac2134f5f625e6d79638ed22ca982ad81cb6
Zach LeClaire <33380765+QuantumCat6561@users.noreply.github.com>
2018-03-11 15:10:14 -0700

    Updating Values

--- Intake.h
+++ Intake.h
@@ -20,9 +20,9 @@ public:
 	void Stop();
 
 private:
-	const float Motor_Speed_Intake  		=  1.0;
-	const float Motor_Speed_Outtake 		= -1.0;
-	const float Motor_Speed_Stop    		=  0.0;
+	const float Motor_Speed_Intake  		=   1.0;
+	const float Motor_Speed_Outtake 		= -0.65;
+	const float Motor_Speed_Stop    		=   0.0;
 	Spark			*MotorIntake1;
 	Spark 			*MotorIntake2;
 };
